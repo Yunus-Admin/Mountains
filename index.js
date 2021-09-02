8bfba71520badab16a774cb08acef4a468694fe8
Yunus <seytnazarov.yunus@gmail.com>
2021-09-02 15:21:21 +0300

    Mountain View

--- index.js
+++ index.js
@@ -8,25 +8,9 @@ function numberDrawHight(count) {
   return sum;
 }
 
-function drawMountain(count) {
-  let resultNumberDrawHight = '';
-
-  for (let i = 0; i < count; i++) {
-    console.log(numberDrawHight(i) + '\\');
-  }
-
-  console.log(numberDrawHight(count) + '|');
-
-  for (let i = count - 1; i >= 0; i--) {
-    console.log(numberDrawHight(i) + '/');
-  }
-}
-
-drawMountain(userNumber);
-
 const numberOfMountains = 5;
 const minН = 3;
-const maxH = 7;
+const maxH = 10;
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -34,6 +18,47 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-for (let i = 0; i <= numberOfMountains; i++) {
-  drawMountain(getRandomIntInclusive(minН, maxH));
+function generateMountain(count, modifications) {
+  const resultNumberDrawHight = [];
+
+  for (let i = 0; i < count; i++) {
+    let add = numberDrawHight(i) + '\\';
+
+    if (modifications) {
+      const mods = ['=', '-*', '===@', ']-+', '◆'];
+      const modPosition = getRandomIntInclusive(0, mods.length - 1);
+      const mod = mods[modPosition];
+      add += mod;
+    }
+
+    resultNumberDrawHight.push(add);
+  }
+
+  resultNumberDrawHight.push(numberDrawHight(count) + '|');
+
+  for (let i = count - 1; i >= 0; i--) {
+    resultNumberDrawHight.push(numberDrawHight(i) + '/');
+  }
+
+  return resultNumberDrawHight;
+}
+
+function drawMountain(count) {
+  generateMountain(count).forEach((item) => {
+    console.log(item);
+  });
+}
+
+function drawMountainRandom(count) {
+  generateMountain(count)
+    .slice(0, getRandomIntInclusive(minН, maxH))
+    .forEach((item) => {
+      console.log(item);
+    });
+}
+
+drawMountain(userNumber);
+
+for (let i = 0; i < numberOfMountains; i++) {
+  drawMountainRandom(getRandomIntInclusive(minН, maxH));
 }
